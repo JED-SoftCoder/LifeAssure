86c41b88818ad1318e75fc7973a8993083e7bddc
Jacob Dillard <jacobedillard@gmail.com>
2021-10-23 10:38:29 -0400

    Added Base code

---
+++ LifeAssure.Data/Agent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeAssure.Data
+{
+    public class Agent
+    {
+        [Key]
+        public int AgentId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int LengthOfEmployment { get; set; }
+        public int NumberOfCustomers { get; set; }
+        public int NumberOfPolicies { get; set; }
+    }
+}
