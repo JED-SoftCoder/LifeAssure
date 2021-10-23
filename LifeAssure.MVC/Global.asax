2a8418d4669f281ba064988a80042e59305c5be3
Jacob Dillard <jacobedillard@gmail.com>
2021-10-23 10:20:33 -0400

    Add project files.

---
+++ LifeAssure.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LifeAssure.MVC.MvcApplication" Language="C#" %>
---
+++ LifeAssure.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LifeAssure.MVC.Startup))]
+namespace LifeAssure.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
