c6c4ba724d9c70f480fcaa14f0bbe81b8160cbf4
Jacob Dillard <jacobedillard@gmail.com>
2021-10-28 09:46:00 -0400

    Added Customer and Policy Class

---
+++ LifeAssure.Data/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeAssure.Data
+{
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        [ForeignKey(nameof(Agent))]
+        public int? AgentId { get; set; }
+        public virtual Agent Agent { get; set; }
+        [ForeignKey(nameof(Policy))]
+        public int? PolicyId { get; set; }
+        public virtual Policy Policy { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int PhoneNumber { get; set; }
+        [Required]
+        public string Address { get; set; }
+        public int YearsOfPolicy { get; set; }
+    }
+}
---
+++ LifeAssure.Data/Policy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeAssure.Data
+{
+    public class Policy
+    {
+        [Key]
+        public int PolicyId { get; set; }
+        [ForeignKey(nameof(Agent))]
+        public int AgentId { get; set; }
+        public virtual Agent Agent { get; set; }
+        [ForeignKey(nameof(Customer))]
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+        [Required]
+        public Guid TypeOfPolicy { get; set; }
+        [Required]
+        public double PolicyAmount { get; set; }
+        [Required]
+        public string Details { get; set; }
+    }
+}
